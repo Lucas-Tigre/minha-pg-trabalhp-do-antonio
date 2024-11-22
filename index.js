41178d2dbaad1b95ee38447a463c9db321a9433b
Lucas-Guercheski <168201998+Lucas-Tigre@users.noreply.github.com>
2024-11-22 00:25:22 -0300

    teset

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
 
+    const email = document.getElementById('email').value;
+    document.getElementById('message').innerText = `inscrição feita com sucesso : ${email}`;
+});
